fda051a64c8ef1dd3e1d4af803bd8533367b790a
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-12-16 21:11:30 +0100

    Improve table formatting

--- zad8/test-app/src/typst.js
+++ zad8/test-app/src/typst.js
@@ -1,9 +1,10 @@
-/** @typedef {{ columns: number; align?: string; header?: string; }} TypstTableOptions */
+/** @typedef {{ columns: number; align?: string; fill?: string; header?: string; }} TypstTableOptions */
 /** @typedef {{ rowspan?: number; colspan?: number; bold?: boolean; mono?: boolean; }} TypstCellOptions */
 
 export class TypstTable {
   #columns;
   #align;
+  #fill;
   #header;
   #cells;
 
@@ -11,6 +12,7 @@ export class TypstTable {
   constructor(options) {
     this.#columns = options.columns;
     this.#align = options.align ?? "auto";
+    this.#fill = options.fill ?? "none";
     this.#header = options.header ?? "";
     this.#cells = [];
   }
@@ -36,13 +38,19 @@ export class TypstTable {
   }
 
   render() {
+    let code = "";
+    for (let i = 0; i < this.#cells.length; i++) {
+      code += this.#cells[i];
+      code += (i + 1) % this.#columns === 0 ? ",\n  " : ", ";
+    }
     return `
 ${this.#header}
 #table(
   columns: ${this.#columns},
   align: ${this.#align},
-  ${this.#cells.join(", ")}
-)`.trimStart();
+  fill: ${this.#fill},
+  ${code}
+)\n`.trimStart();
   }
 }
 
@@ -50,9 +58,10 @@ export function initializeDiffTable() {
   const table = new TypstTable({
     columns: 7,
     align: "right + horizon",
-    header: `#let diff(n) = if n == 0 { [#n] } else if n > 0 { text(fill: rgb("#880000"))[+#n] } else { text(fill: rgb("#008800"))[-#calc.abs(n)] }`,
+    fill: `(x, y) => if y in (0, 1, 9) { rgb("#cce") } else if calc.rem(y, 2) == 0 { rgb("#f0f0ff") }`,
+    header: `#let r(n) = text(fill: rgb("#880000"), n)\n#let g(n) = text(fill: rgb("#008800"), n)`,
   });
-  table.addCell("Name", { rowspan: 2, bold: true });
+  table.addCell("Nazwa", { rowspan: 2, bold: true });
   table.addCell("Średni czas [ms]", { colspan: 3, bold: true });
   table.addCell("Koszt", { colspan: 3, bold: true });
   table.addCell("Stary", { bold: true });
@@ -62,4 +71,29 @@ export function initializeDiffTable() {
   table.addCell("Nowy", { bold: true });
   table.addCell("Zmiana", { bold: true });
   return table;
+}
+
+/**
+ * @param {number} n
+ * @param {boolean} int
+ */
+export function f(n, int = false) {
+  return n
+    .toLocaleString("en", {
+      minimumFractionDigits: int ? 0 : 2,
+      maximumFractionDigits: int ? 0 : 2,
+    })
+    .replaceAll(",", " ");
+}
+
+/**
+ * @param {number} n
+ * @param {boolean} int
+ */
+export function diff(n, int = false) {
+  return n === 0
+    ? f(n, int)
+    : n > 0
+    ? `#r("+${f(n, int)}")`
+    : `#g("-${f(Math.abs(n), int)}")`;
 }
