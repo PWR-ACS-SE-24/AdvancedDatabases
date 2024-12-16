60d6e803a6016be4ad227ce5300d9c9f1a0beb9b
jakubzehner <kubusja.pl@gmail.com>
2024-12-16 13:00:46 +0100

    new results

--- zad8/test-app/src/main.js
+++ zad8/test-app/src/main.js
@@ -13,7 +13,7 @@ import { initializeDiffTable } from "./typst.js";
 const con = await oracledb.getConnection({
   user: "system",
   password: "password",
-  connectString: "localhost:1521/XEPDB1",
+  connectString: "172.27.240.1:1521/XEPDB1",
   stmtCacheSize: 0,
 });
 
